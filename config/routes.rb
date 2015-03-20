1139ef1ca67994b74e77f2489a7f1f27775bf096
Tho Nguyen <thogotmail@gmail.com>
2015-03-20 15:03:33 -0500

    add routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get '/register', to: 'users#new'
   get '/forum', to: 'topics#index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
   resources :users, only: [:create]
 end
