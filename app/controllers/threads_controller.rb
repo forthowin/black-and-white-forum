f22fbd5852a7a68e804baaf8a3db793e16979dba
Tho Nguyen <thogotmail@gmail.com>
2015-03-21 23:32:34 -0500

    Test and implement create new subject

--- app/controllers/threads_controller.rb
+++ app/controllers/threads_controller.rb
@@ -1,4 +1,6 @@
 class ThreadsController < ApplicationController
+  before_action :require_user, only: [:new, :create]
+
   def index
     @topic = Topic.find(params[:topic_id])
     @threads = @topic.subjects
@@ -8,5 +10,25 @@ class ThreadsController < ApplicationController
     @topic = Topic.find params[:topic_id]
     @thread = Subject.find params[:thread_id]
     @post = Post.new
+  end
+
+  def new
+    @topic = Topic.find params[:topic_id]
+    @subject = Subject.new
+  end
+
+  def create
+    @subject = Subject.new(subject_params)
+    @subject.user = current_user
+    @subject.topic_id = params[:topic_id]
+    @subject.save
+    flash[:success] = "Subject has been created."
+    redirect_to forum_thread_path(params[:topic_id], @subject.id)
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:title, :body)
   end
 end
--- spec/controllers/threads_controller_spec.rb
+++ spec/controllers/threads_controller_spec.rb
@@ -29,4 +29,63 @@ describe ThreadsController do
       expect(assigns(:topic)).to eq(topic)
     end
   end
+
+  describe "GET new" do
+    it "assigns @subject" do
+      bob = Fabricate(:user)
+      session[:user_id] = bob.id
+      topic = Fabricate(:topic)
+      get :new, topic_id: topic.id
+      expect(assigns(:subject)).to be_a_new(Subject)
+    end
+
+    it "assigns @topic" do
+      bob = Fabricate(:user)
+      session[:user_id] = bob.id
+      topic = Fabricate(:topic)
+      get :new, topic_id: topic.id
+      expect(assigns(:topic)).to eq(topic)
+    end
+
+    it "redirects to login page for unauthenticated users" do
+      topic = Fabricate(:topic)
+      get :new, topic_id: topic.id
+      expect(response).to redirect_to login_path
+    end
+  end
+
+  describe "POST create" do
+    context "with valid inputs" do
+      it "redirects to login page for unauthenticated users" do
+        topic = Fabricate(:topic)
+        bob = Fabricate(:user)
+        post :create, subject: {title: 'test', body: 'asdf'}, topic_id: topic.id
+        expect(response).to redirect_to login_path
+      end
+
+      it "creates a new subject" do
+        bob = Fabricate(:user)
+        session[:user_id] = bob.id
+        topic = Fabricate(:topic)
+        post :create, subject: {title: 'title', body: 'body'}, topic_id: topic.id
+        expect(Subject.count).to eq(1)
+      end
+
+      it "sets the flash success message" do
+        bob = Fabricate(:user)
+        session[:user_id] = bob.id
+        topic = Fabricate(:topic)
+        post :create, subject: {title: 'title', body: 'body'}, topic_id: topic.id
+        expect(flash[:success]).to be_present
+      end
+
+      it "redirects to the show subject path" do
+        bob = Fabricate(:user)
+        session[:user_id] = bob.id
+        topic = Fabricate(:topic)
+        post :create, subject: {title: 'title', body: 'body'}, topic_id: topic.id
+        expect(response).to redirect_to forum_thread_path(topic.id, 1)
+      end
+    end
+  end
 end
