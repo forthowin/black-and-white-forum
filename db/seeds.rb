ccce3fdbf5b173f3b7f02a8f37aab8dc2eef3e4c
Tho Nguyen <thogotmail@gmail.com>
2015-03-23 15:19:36 -0500

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-bill = User.create(username: 'bill', password: 'password')
+Topic.create(title: 'General', description: 'Anything non clash related.')
+Topic.create(title: 'Clan War', description: 'Talk strategy about current war.')
+Topic.create(title: 'Tactics and Strategy', description: 'Theorycraft base designs and attack compositions.')
+Topic.create(title: 'Introductions', description: 'Introduce yourself!')
+Topic.create(title: 'Clan Application', description: 'Want to join Black and White? Post why you should be recruited!')
+Topic.create(title: 'Suggestions', description: 'Got an idea on how to impove the clan or the website? Post here!')
 
-clan_war = Topic.create(title: 'Clan War', description: 'Talk strategy about current war.')
-strat = Topic.create(title: 'Tactics and Strategy', description: 'Theorycraft base designs and attack compositions.')
-general = Topic.create(title: 'General', description: 'Anything non clash related.')
 
-thread1 = Subject.create(title: 'I need help with base design', body: 'My base is bad and need a rework, help!', user: bill, topic: strat)
-thread2 = Subject.create(title: 'What is the best army comp', body: 'Is it Gowiwi?', user: bill, topic: strat)
-thread3 = Subject.create(title: 'What do you guys think about this base?', body: 'Its pretty good i think.', user: bill, topic: strat)
+# thread1 = Subject.create(title: 'I need help with base design', body: 'My base is bad and need a rework, help!', user: bill, topic: strat)
+# thread2 = Subject.create(title: 'What is the best army comp', body: 'Is it Gowiwi?', user: bill, topic: strat)
+# thread3 = Subject.create(title: 'What do you guys think about this base?', body: 'Its pretty good i think.', user: bill, topic: strat)
 
-Post.create(body: 'yeah its pretyt oasd foasdfoasd foasdfj oas fos fasf aosdf.', user: bill, subject: thread2)
-Post.create(body: 'yasdfasdf eah its pretyt oasd foasdfoasd foasdfj oas fos fasf aosdf.', user: bill, subject: thread2)
-Post.create(body: 'yesdfah its pretysdft oasd foassa dfasfdfdfoa asdfasdsd foasdfj oas fos fasf aosdf.', user: bill, subject: thread2)
+# Post.create(body: 'yeah its pretyt oasd foasdfoasd foasdfj oas fos fasf aosdf.', user: bill, subject: thread2)
+# Post.create(body: 'yasdfasdf eah its pretyt oasd foasdfoasd foasdfj oas fos fasf aosdf.', user: bill, subject: thread2)
+# Post.create(body: 'yesdfah its pretysdft oasd foassa dfasfdfdfoa asdfasdsd foasdfj oas fos fasf aosdf.', user: bill, subject: thread2)
