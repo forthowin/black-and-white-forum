ddc55eb3ed2c6d05f08d90e68f3891b4251df165
Tho Nguyen <thogotmail@gmail.com>
2015-03-23 17:34:20 -0500

    fix for first time viewing page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,6 +38,7 @@ module ApplicationHelper
   end
 
   def normalize_post_number(index)
+    params[:page] ||= 1
     index + 1 + 10 * (params[:page].to_i - 1)
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -26,5 +26,9 @@ describe ApplicationHelper do
       params[:page] = 2
       expect(normalize_post_number(0)).to eq(11)
     end
+
+    it "returns 1 for the first post on the first page when viewing for the first time" do
+      expect(normalize_post_number(0)).to eq(1)
+    end
   end
 end
