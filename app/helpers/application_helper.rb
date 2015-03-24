ea72c4540b3732e85cb66823f0effd9cbd7c1c0e
Tho Nguyen <thogotmail@gmail.com>
2015-03-23 19:26:13 -0500

    replace custom time difference with rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,42 +1,4 @@
 module ApplicationHelper
-  MINUTE_IN_SECONDS = 60
-  HOUR_IN_SECONDS = MINUTE_IN_SECONDS * 60
-  DAY_IN_SECONDS = HOUR_IN_SECONDS * 24
-  MONTH_IN_SECONDS = DAY_IN_SECONDS * 30
-  YEAR_IN_SECONDS = MONTH_IN_SECONDS * 12
-
-  def time_difference(dt)
-    diff = TimeDifference.between(Time.zone.now, dt).in_seconds
-
-    case
-    when diff < MINUTE_IN_SECONDS
-      "#{diff.to_i} " + "second".pluralize(diff.to_i) + " ago"
-    when diff.between?(MINUTE_IN_SECONDS, HOUR_IN_SECONDS)
-      diff = TimeDifference.between(Time.zone.now, dt).in_minutes.to_i
-      "#{diff} " + "minute".pluralize(diff) + " ago"
-    when diff.between?(HOUR_IN_SECONDS, DAY_IN_SECONDS)
-      diff = TimeDifference.between(Time.zone.now, dt).in_hours.to_i
-      "#{diff} " + "hour".pluralize(diff) + " ago"
-    when diff.between?(DAY_IN_SECONDS, MONTH_IN_SECONDS)
-      diff = TimeDifference.between(Time.zone.now, dt).in_days.to_i
-      "#{diff} " + "day".pluralize(diff) + " ago"
-    when diff.between?(MONTH_IN_SECONDS, YEAR_IN_SECONDS)
-      diff = TimeDifference.between(Time.zone.now, dt).in_months.to_i
-      "#{diff} " + "month".pluralize(diff) + " ago"
-    when diff > YEAR_IN_SECONDS
-      diff = TimeDifference.between(Time.zone.now, dt).in_years.to_i
-      "#{diff} " + "year".pluralize(diff) + " ago"
-    end
-  end
-
-  def display_recent_subject_or_post_time(subject)
-    if subject.posts.empty?
-      time_difference(subject.created_at)
-    else
-      time_difference(subject.posts.last.created_at)
-    end
-  end
-
   def normalize_post_number(index)
     params[:page] ||= 1
     index + 1 + 10 * (params[:page].to_i - 1)
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,21 +1,6 @@
 require 'rails_helper'
 
 describe ApplicationHelper do
-  describe "#time_difference" do
-    let(:bob) { Fabricate(:user) }
-    let(:topic) { Fabricate(:topic) }
-
-    it "returns a created ago string in seconds" do
-      subject = Fabricate(:subject, user: bob, topic: topic, created_at: 1.second.ago)
-      expect(time_difference(subject.created_at)).to eq("1 second ago")
-    end
-
-    it "pluralizes seconds if it is greater than 1" do
-      subject = Fabricate(:subject, user: bob, topic: topic, created_at: 2.second.ago)
-      expect(time_difference(subject.created_at)).to eq("2 seconds ago")
-    end
-  end
-
   describe "#normalize_post_number" do
     it "returns 1 for the first post on the first page" do
       params[:page] = 1
